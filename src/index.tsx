5564800bac0d3279a11a096c0b992f47ce353334
Damian Silbergleith <dcunniff@gmail.com>
2022-08-29 06:51:18 -0700

    fix type issue in index

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ if (config.SENTRY_URI) {
 }
 
 Raven.context(function() {
-  const container = document.getElementById("root");
+  const container = document.getElementById("root") as HTMLElement;
   const root = createRoot(container);
   root.render(<App />);
 });
